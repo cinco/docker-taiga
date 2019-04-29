d14726a17d1e21a6f86a8ecbfa2c8ddf2e91436a
Thiago Euzebio <euzebiotre@decea.gov.br>
2019-04-29 13:08:43 -0300

    ajusta sintaxe de local.py

--- conf/taiga/local.py
+++ conf/taiga/local.py
@@ -13,15 +13,15 @@ from .docker import *
 PUBLIC_REGISTER_ENABLED = False
 DEBUG = False
 TEMPLATE_DEBUG = False
-TAIGA_HOSTNAME=pista.atdsdop.com
+TAIGA_HOSTNAME='pista.atdsdop.com'
 TAIGA_SSL_BY_REVERSE_PROXY='True'
 # TAIGA_SSL=True (see Enabling HTTPS below)
-TAIGA_SECRET_KEY=oCy7cWMjESJYesKPaitUETYU7ZfsCodz2UpcWiiytoPY6wFZYXhFHDrynr3bHo
+TAIGA_SECRET_KEY='oCy7cWMjESJYesKPaitUETYU7ZfsCodz2UpcWiiytoPY6wFZYXhFHDrynr3bHo'
 # TAIGA_SKIP_DB_CHECK (set to skip the database check that attempts to automatically setup initial database)
 TAIGA_ENABLE_EMAIL='true'
-TAIGA_EMAIL_FROM=web@atdsdop.com
+TAIGA_EMAIL_FROM='web@atdsdop.com'
 TAIGA_EMAIL_USE_TLS='true'
-TAIGA_EMAIL_HOST=box.cincoeuzebio.com
-TAIGA_EMAIL_PORT=587
-TAIGA_EMAIL_USER=web@atdsdop.com
+TAIGA_EMAIL_HOST='box.cincoeuzebio.com'
+TAIGA_EMAIL_PORT='587'
+TAIGA_EMAIL_USER='web@atdsdop.com'
 TAIGA_EMAIL_PASS='4dm1n4TD'
